6c3d8e813562bcf76280937f56e41f79860a9c73
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-04-13 10:28:36 +0700

    DEBUG: Make indexer verbose

--- src/Service/Client.php
+++ src/Service/Client.php
@@ -59,7 +59,7 @@ class Client
         error_log(exec('ls -lh /var/lib/sphinxsearch/data'));
         error_log(exec('whoami'));
 
-        error_log(exec('/usr/bin/indexer --all'));
+        error_log(exec('/usr/bin/indexer --verbose --all'));
         error_log('After indexing');
         error_log(exec('ls -lh /var/lib/sphinxsearch/data'));
         error_log(exec('cat /etc/sphinxsearch/sphinx.conf'));
